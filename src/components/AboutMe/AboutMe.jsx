2b90c4405143f259d1d39ca57d8014513818e668
denys_gl <oggyla@gmail.com>
2020-12-08 18:30:55 -0800

    type fix

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -18,7 +18,7 @@ const AboutMe = () => {
             <div className="row">
               <div className="col">
                 <p>
-                  Frontend/Fullstack with experience in adaptive development and counseling as a frontend developer at Areon Consulting in Ukraine, where I introduced new designs using Sketch with support for outdated as well as current devices that increased users quantity by 35% and user experience quality by 28%. During my time at Areon Consulting, we also introduced push notifications using hybrid technology (Ionic) for mobile devices.
+                  Frontend/Fullstack developer with experience in adaptive development and counseling as a frontend developer at Areon Consulting in Ukraine, where I introduced new designs using Sketch with support for outdated as well as current devices that increased users quantity by 35% and user experience quality by 28%. During my time at Areon Consulting, we also introduced push notifications using hybrid technology (Ionic) for mobile devices.
                 </p>
                 <p>
                   Most recently, I sharpened my skills in MERN, C#, and Python at Coding Dojo in pursuit of developing higher quality products.
