9e2eced6f40c30e7bec277c7f2caa9a6abab13e2
denys_gl <oggyla@gmail.com>
2020-12-10 17:12:06 -0800

    added simple footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="container-fluid purple-bkg pt-4 pb-4">
+      <div className="row">
+        <div className="col text-center">
+          <p className="">
+            <i class="far fa-copyright"></i>
+            <h4 className="d-inline pl-3 mb-0 align-bottom">Denys Glukhovskyi</h4>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
